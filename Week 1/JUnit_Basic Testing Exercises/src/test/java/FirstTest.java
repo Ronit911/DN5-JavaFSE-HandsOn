c786f154eda407b009be91bb9bd4806e80d4c6a0
Ronit <mehtaronit11@gmail.com>
2026-06-24 11:55:54 +0530

    Exercise 1: Setting Up JUnit

---
+++ Week 1/JUnit_Basic Testing Exercises/src/test/java/FirstTest.java
@@ -0,0 +1,9 @@
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FirstTest {
+    @Test
+    public void testSetup() {
+        assertTrue("JUnit is set up correctly!", true);
+    }
+}
