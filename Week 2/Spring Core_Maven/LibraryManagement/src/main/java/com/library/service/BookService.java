a2ee090f6902a63acaaf929d04c93e3ed430f140
Ronit <mehtaronit11@gmail.com>
2026-06-24 14:46:10 +0530

    Implement Dependency Injection: wire BookRepository into BookService

--- Week 2/Spring Core_Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week 2/Spring Core_Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,7 +1,18 @@
 package com.library.service;
 
+import com.library.repository.BookRepository;
+
 public class BookService {
+    private BookRepository bookRepository;
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
     public void serveBooks() {
         System.out.println("Serving books from the library...");
+        if (bookRepository != null) {
+            bookRepository.getBooks();
+        }
     }
 }
