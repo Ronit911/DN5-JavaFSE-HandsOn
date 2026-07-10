530268cf8f98b43595c6a43feb3988ce6a5fe0cb
Ronit <mehtaronit11@gmail.com>
2026-07-10 17:19:14 +0530

    Add files via upload

--- Week 3/1. spring-rest-handson/spring-learn/src/main/java/com/cognizant/springlearn/Country.java
+++ Week 3/1. spring-rest-handson/spring-learn/src/main/java/com/cognizant/springlearn/Country.java
@@ -1,6 +1,14 @@
 package com.cognizant.springlearn;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Country {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Country.class);
     private String code;
     private String name;
+
+    public Country() {
+        LOGGER.debug("Inside Country Constructor.");
+    }
 }
