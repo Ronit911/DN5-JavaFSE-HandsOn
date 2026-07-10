2e25c82fa9fc9ae6ba888d39e71297e262ce5ece
Ronit <mehtaronit11@gmail.com>
2026-07-10 22:08:09 +0530

    Create SecurityConfig.java to configure authorization

---
+++ Week 3/5. JWT-handson/spring-learn/src/main/java/com/cognizant/springlearn/security/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.cognizant.springlearn.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+            .passwordEncoder(NoOpPasswordEncoder.getInstance())
+            .withUser("user").password("pwd").roles("USER")
+            .and()
+            .withUser("admin").password("pwd").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+            .authorizeRequests()
+            .antMatchers("/countries").hasRole("USER")
+            .antMatchers("/authenticate").hasAnyRole("USER", "ADMIN")
+            .anyRequest().permitAll()
+            .and()
+            .httpBasic();
+    }
+}
